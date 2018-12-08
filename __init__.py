9b7a24258a285b48904a763fa741fd826eb6090b
b <vpbalint@silentsignal.hu>
2018-12-08 11:05:46 +0100

    Comparing in a background thread

--- __init__.py
+++ __init__.py
@@ -310,106 +310,118 @@ def get_func_predecessors(bv,f):
             ret.append(x_func.start)
     return ret
 
-def match_fvs(data0, data1):
-    res=[]
-    ordered_keys0=sorted(data0,key=data0.get,reverse=True)
-    ordered_keys1=sorted(data1,key=data1.get,reverse=True)
 
-    for func0 in ordered_keys0: # So we can delete elements
-        if func0 not in data0: continue
-        feat0 = data0[func0]
+class SimilarNinjaComparer(BackgroundTaskThread):
+    def __init__(self, text=""):
+        super(SimilarNinjaComparer, self).__init__(text, False)
 
-        for func1 in ordered_keys1:
-            if func1 not in data1: continue
-            feat1 = data1[func1]
-            matching=True
-            for i in xrange(0,len(feat1)):
-                if feat0[i] != feat1[i]:
-                    matching=False
+
+    def match_fvs(self, data0, data1):
+        res=[]
+        ordered_keys0=sorted(data0,key=data0.get,reverse=True)
+        ordered_keys1=sorted(data1,key=data1.get,reverse=True)
+
+        for func0 in ordered_keys0: # So we can delete elements
+            if func0 not in data0: continue
+            feat0 = data0[func0]
+
+            for func1 in ordered_keys1:
+                if func1 not in data1: continue
+                feat1 = data1[func1]
+                matching=True
+                for i in xrange(0,len(feat1)):
+                    if feat0[i] != feat1[i]:
+                        matching=False
+                        break
+                if matching:
+                    log_info("%x <-> %x %s (%f)\n%s %s" % (long(func0), long(func1), [], 1.0, feat0, feat1))
+                    res.append(((long(func0), feat0), (long(func1),feat1), 1.0))
+                    del data0[func0]
+                    del data1[func1]
                     break
-            if matching:
-                log_info("%x <-> %x %s (%f)\n%s %s" % (long(func0), long(func1), [], 1.0, feat0, feat1))
-                res.append(((long(func0), feat0), (long(func1),feat1), 1.0))
-                del data0[func0]
-                del data1[func1]
-                break
-    return res        
+        return res        
 
-def compare_data(bv):
-    fn0 = get_open_filename_input("filename0:","*")
-    fn1 = get_open_filename_input("filename1:","*")
 
-    data0 = None
-    data1 = None
-    
-    if fn0.endswith(".bndb"):
-        fm = FileMetadata()
-        db0 = fm.open_existing_database(fn0)
-        for t in db0.available_view_types:
-            try:
-                bv0= db0.get_view_of_type(t.name)
-                data0 = json.loads(bv0.query_metadata("similarninja"))
-                break
-            except KeyError:
-                pass
-    else:
-        f0 = open(fn0, "r")
-        data0=json.loads(f0.read())
-    
-    if fn1.endswith(".bndb"):
-        fm = FileMetadata()
-        db1 = fm.open_existing_database(fn1)
-        for t in db1.available_view_types:
-            try:
-                bv1 = db1.get_view_of_type(t.name)
-                data1 = json.loads(bv1.query_metadata("similarninja"))
-                break
-            except KeyError:
-                pass
-    else:
-        f1 = open(fn1, "r")
-        data1=json.loads(f1.read())
+    def run(self):        
+        self.progress="Opening files for comparison"
 
-    log_info("Data sizes: %d %d" % (len(data0), len(data1)))
-    matches=match_fvs(data0, data1)
-    log_info("Data sizes after matching: %d %d" % (len(data0), len(data1)))
-    
-    # Inexact matches
-    for func0 in list(data0.keys()): # So we can delete elements
-        if func0 not in data0: continue
-        feat0 = data0[func0]
+        fn0 = get_open_filename_input("filename0:","*")
+        fn1 = get_open_filename_input("filename1:","*")
 
-        sims0 = [None] * len(PROVIDERS)
-        sim_avg0=0.0
-        func_match=None
-        feat_match=None
-        for func1 in list(data1.keys()):
-            if func1 not in data1: continue
-            feat1 = data1[func1]
-
-            sims = [None] * len(PROVIDERS)
-            for i, p in enumerate(PROVIDERS):
-                sims[i]=p.compare(feat0[i],feat1[i])
-            sim_avg=0.0
-            for s in sims:
-                sim_avg += s
-            sim_avg = sim_avg / len(sims)        
-
-            if sim_avg > sim_avg0:
-                sim_avg0=sim_avg
-                sims0=sims
-                func_match=func1
-                feat_match=feat1
-            if sim_avg0 == 1.0: break # Exit early for perfect matches
+        data0 = None
+        data1 = None
         
-        log_info("%x <-> %x %s (%f)\n%s %s" % (long(func0), long(func_match), repr(sims0), sim_avg0, feat0, feat_match))
-        matches.append(((long(func0), feat0), (long(func_match),feat_match), sim_avg0))
-        del data0[func0]
-        del data1[func_match]
-    out = open(get_save_filename_input("Filename to save comparison results:","*","compare.json"),"wb")
-    out.write(json.dumps(matches))
-    out.close()
-    return matches
+        if fn0.endswith(".bndb"):
+            fm = FileMetadata()
+            db0 = fm.open_existing_database(fn0)
+            for t in db0.available_view_types:
+                try:
+                    bv0= db0.get_view_of_type(t.name)
+                    data0 = json.loads(bv0.query_metadata("similarninja"))
+                    break
+                except KeyError:
+                    pass
+        else:
+            f0 = open(fn0, "r")
+            data0=json.loads(f0.read())
+        
+        if fn1.endswith(".bndb"):
+            fm = FileMetadata()
+            db1 = fm.open_existing_database(fn1)
+            for t in db1.available_view_types:
+                try:
+                    bv1 = db1.get_view_of_type(t.name)
+                    data1 = json.loads(bv1.query_metadata("similarninja"))
+                    break
+                except KeyError:
+                    pass
+        else:
+            f1 = open(fn1, "r")
+            data1=json.loads(f1.read())
+
+        self.progress="Comparing..."
+
+        log_info("Data sizes: %d %d" % (len(data0), len(data1)))
+        matches=self.match_fvs(data0, data1)
+        log_info("Data sizes after matching: %d %d" % (len(data0), len(data1)))
+        
+        # Inexact matches
+        for func0 in list(data0.keys()): # So we can delete elements
+            if func0 not in data0: continue
+            feat0 = data0[func0]
+
+            sims0 = [None] * len(PROVIDERS)
+            sim_avg0=0.0
+            func_match=None
+            feat_match=None
+            for func1 in list(data1.keys()):
+                if func1 not in data1: continue
+                feat1 = data1[func1]
+
+                sims = [None] * len(PROVIDERS)
+                for i, p in enumerate(PROVIDERS):
+                    sims[i]=p.compare(feat0[i],feat1[i])
+                sim_avg=0.0
+                for s in sims:
+                    sim_avg += s
+                sim_avg = sim_avg / len(sims)        
+
+                if sim_avg > sim_avg0:
+                    sim_avg0=sim_avg
+                    sims0=sims
+                    func_match=func1
+                    feat_match=feat1
+                if sim_avg0 == 1.0: break # Exit early for perfect matches
+            
+            log_info("%x <-> %x %s (%f)\n%s %s" % (long(func0), long(func_match), repr(sims0), sim_avg0, feat0, feat_match))
+            matches.append(((long(func0), feat0), (long(func_match),feat_match), sim_avg0))
+            del data0[func0]
+            del data1[func_match]
+        self.finish()
+        out = open(get_save_filename_input("Filename to save comparison results:","*","compare.json"),"wb")
+        out.write(json.dumps(matches))
+        out.close()
+        return matches
 
 def tester(bv0,bv1,result_file):
     matches=json.loads(open(result_file,"r").read())
@@ -440,6 +452,9 @@ def tester(bv0,bv1,result_file):
     log_info("Failure: %d" % failure)
     log_info("Total: %d (%d <-> %d) " % (len(matches), len(bv0.functions), len(bv1.functions)))
 
+def compare(bv):
+    sn_comparer=SimilarNinjaComparer()
+    sn_comparer.start()
 
 PluginCommand.register("SimilarNinja - Generate Feature Vectors", "Generates Feature Vectors for all functions", gen_feature)
-PluginCommand.register("SimilarNinja - Compare", "Compare functions based on generated data files", compare_data)
+PluginCommand.register("SimilarNinja - Compare", "Compare functions based on generated data files", compare)
